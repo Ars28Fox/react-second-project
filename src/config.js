7ceef88b9effc0432cb745f6e872ef46016b7d26
Арсений Лисин <lisin@texterra.local>
2021-05-26 16:39:31 +0300

    {

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY);
 
-const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru';
+const API_URL = 'https://fortniteapi.io/v1/shop?lang=ru';
 
 export {
     API_KEY,
