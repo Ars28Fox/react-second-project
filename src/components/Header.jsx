53c7b36f6f3396d71bba9bf0edd10a5318d20441
Арсений Лисин <lisin@texterra.local>
2021-05-26 10:53:45 +0300

    init project template

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="purple darken-4">
+        <div className="nav-wrapper">
+            <a href="!#" className="brand-logo right">React Store</a>
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
+                <li><a href="!#">GitHub Repo</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export { Header }
